b761f8c5f206850dd70f3d18c6b7266d4a3a709b
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-05-05 23:40:31 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -7,25 +7,24 @@ public:
     
 	cout<<"Function with 0 argument"<<endl; 
 	}
-    void f1(int arg1) 
+            void f1(int arg1) 
 	{ 
 	cout<<"Function with 1 argument: "<<arg1<<endl;
-	 }
-    void f2(int arg1, int arg2) { 
+	}
+            void f2(int arg1, int arg2) { 
 	cout<<"Function with 2 arguments: "<<arg1<<" "<<arg2<<endl;
-	 }
-    void f3(int arg1, int arg2, int arg3) { 
+	}
+            void f3(int arg1, int arg2, int arg3) { 
 	cout<<"Function with 3 arguments: "<< arg1<<" "<<arg2<<" "<<arg3<<endl;
-	 }
-    
-    void f(int arg1 = 0, int arg2 = 0, int arg3 = 0, int arg4 = 0) {
-    cout<<"Function with default arguments: "<<arg1<<" "<<arg2<<" "<<arg3<<" "<<arg4<<endl;
+	}
+            void f(int arg1 = 0, int arg2 = 0, int arg3 = 0, int arg4 = 0) {
+        cout<<"Function with default arguments: "<<arg1<<" "<<arg2<<" "<<arg3<<" "<<arg4<<endl;
     }
 };
 
 int main() {
     
-	A obj;
+    A obj;
     
     obj.f0();
     obj.f1(10);
