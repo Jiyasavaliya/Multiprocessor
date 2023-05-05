2fd724ca44d2e3e03b40916b1884524a81e2c4ea
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-05-05 21:13:35 +0530

    Update unary operator.cpp

--- unary operator.cpp
+++ unary operator.cpp
@@ -34,51 +34,7 @@ class B{
 	}
 };
 int main(){
-	A obj1,obj2;
-	cout<<"Increment"<<endl;
-	obj1.set(1);
-	obj1.get();
-	obj2 = ++obj1;
-	obj2.get();
-	cout<<endl;
 
-	B ob1,ob2;
-	cout<<"Decrement"<<endl;#include<iostream>
-using namespace std;
-class A{
-	private:
-		int a;
-	public:
-		void set(int a1){
-			a=a1;
-		}
-		void get(){
-		 cout<<a<<endl;
-		}
-	   A operator ++ (){
-		A ans;
-		 ans.a=++a;
-		 return ans;
-	}
-};
-class B{
-	private:
-		int b;
-	public:
-		void set(int b1){
-			b=b1;
-		}
-		void get(){
-		 cout<<b<<endl;
-		}
-
-	B operator -- (){
-	    B asn;
-		 asn.b=--b;
-		 return asn;
-	}
-};
-int main(){
 	A obj1,obj2;
 	cout<<"Increment"<<endl;
 	obj1.set(1);
@@ -89,14 +45,6 @@ int main(){
 
 	B ob1,ob2;
 	cout<<"Decrement"<<endl;
-	ob1.set(1);
-	ob1.get();
-	ob2 = --ob1;
-	ob2.get();
-
-	return 0;
-
-}
 	ob1.set(1);
 	ob1.get();
 	ob2 = --ob1;
